fee80e1a33165703e1356b9cbf4b3c886889ddf3
Your Name <you@example.com>
2021-07-12 18:39:13 -0500

    Finished sql 4

--- queries.sql
+++ queries.sql
@@ -8,4 +8,5 @@ SELECT * FROM employer WHERE (location = "Saint Louis, MO");
 ## Part 3: Test it with SQL
 DROP TABLE job;
 ## Part 4: Test it with SQL
-SELECT name, description FROM skill ORDER BY name IS NOT NULL, name ASC;
+SELECT name, description FROM skill INNER JOIN job_skills on skill.id = job_skills.skills_id
+WHERE name IS NOT NULL and description IS NOT NULL ORDER BY name;
